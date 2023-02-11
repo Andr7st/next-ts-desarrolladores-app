4187600a9cf14d5ce0336019a4918b5db7097dd8
Andres Segura <andres.segura.dev@gmail.com>
2023-02-11 13:07:32 -0500

    initial commmit: desarrolladores.app

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NextPage } from 'next'
+
+const HomePage: NextPage = () => {
+  return (
+    <React.Fragment>
+      <h1> Hello world! </h1>
+    </React.Fragment>
+  )
+}
+
+export default HomePage

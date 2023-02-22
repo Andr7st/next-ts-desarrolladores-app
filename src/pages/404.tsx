b5c1b0c74d4c5df920f711a382c7ad8245468f1b
Andres Segura <andres.segura.dev@gmail.com>
2023-02-22 14:38:36 -0500

    add: Page layout-page and hero-section to index page.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,36 @@
 import React from 'react'
-
 import { NextPage } from 'next'
+import PageLayout from '@/containers/PageLayout'
+import { Box, SxProps, Theme, Typography } from '@mui/material'
 
+// SEO Override:
+const seoProps = {
+  title: 'Page 404',
+  description: 'Page Not Found'
+}
+
+// Material UI Override:
+const mainBox: SxProps<Theme> = {
+  minHeight: '73vh',
+  marginTop: 8,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+}
+
+// 
 const Custom404Page: NextPage = () => {
   return (
-    <React.Fragment>
-      <h1> { '404' } </h1>
-    </React.Fragment>
+    <PageLayout seoProps={seoProps} >
+      <Box sx={mainBox}>
+        <Typography variant="h1" display="block" gutterBottom>
+          404
+        </Typography>
+        <Typography variant="subtitle2" gutterBottom>
+          Page not found.
+        </Typography>
+      </Box>
+    </PageLayout>
   )
 }
 
